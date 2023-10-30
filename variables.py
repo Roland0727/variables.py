69198685faf70d97e22ce1bcf2fd937a5dfc2f80
Roland0727 <149345236+Roland0727@users.noreply.github.com>
2023-10-29 23:16:56 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+nombre= ¨Rolando¨
+Edad = 46
+deportista = true
+estatura = 1.71
+print (¨nombre¨,nombre)
+print (ëdad¨, Edad)
+print (¨deportista¨,deportista)
+print (ëstatura¨,estatura)
+#limite de enteros y flotantes en Python
+#Los enteros en Python no tienen un límite de tamaño, lo que significa que pueden ser tan grandes como la memoria disponible en la computadora
+#Por otro lado, los números de punto flotante se representan en el hardware de las computadoras como fracciones en base 2 (binarias) y tienen limitaciones en cuanto a su precisión
+numero_1 = 20
+numero_2 = 40
+suma = numero_1+numero_2
+print(¨suma¨) #este es el Resultado
